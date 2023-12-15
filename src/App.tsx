6adef4209f4734fb1f1ae6da8287a8d815dca4f9
saemii-24 <hims2419@gmail.com>
2023-12-15 19:33:23 +0900

    Fix: Signup button disabled 에러 수정 >> 조건에 충족하지 않은 값으로 회원가입 시도할 경우 button disabled 되는 상황에서 발생한 에러 수정 >> Resolves: #15

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import './App.scss';
 import './common.scss';
 import SideMenu from './component/SideMenu';
 import Login from './pages/login/Login';
+import ConfirmLogin from './pages/login/ConfirmLogin';
 import Signup from './pages/siginup/Signup';
 import './API.js';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/main/Main';
 import MyPage from './pages/mypage/MyPage';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+  const context = useContext(AuthContext);
+
   return (
     <div className='App'>
       <div id='wrap'>
@@ -17,12 +21,23 @@ function App() {
         <div className='content-back'>
           <SideMenu />
           <div className='content'>
-            <Routes>
-              <Route path='/' element={<Main />} />
-              <Route path='/mypage' element={<MyPage />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/signup' element={<Signup />} />
-            </Routes>
+            {context.user ? (
+              <Routes>
+                <Route path='/' element={<Main />} />
+                <Route path='/mypage' element={<MyPage />} />
+                <Route path='/loginconfirm' element={<ConfirmLogin />} />
+                <Route path='/*' element={<Main />} />{' '}
+              </Routes>
+            ) : (
+              <Routes>
+                <Route path='/' element={<Main />} />
+                <Route path='/mypage' element={<MyPage />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/loginconfirm' element={<ConfirmLogin />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/*' element={<Main />} />{' '}
+              </Routes>
+            )}
           </div>
         </div>
       </div>
