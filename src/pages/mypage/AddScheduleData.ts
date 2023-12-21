7320b4f67a80df133e42ba3182d0d693480dc369
saemii-24 <hims2419@gmail.com>
2023-12-21 20:43:10 +0900

    Feat: Schedule 로그인 분기 처리 로그인 하지 않았을 경우 표시 방식 변경, 로그인 하지 않았을 경우의 문장 데이터 추가 Ref: #13 Resolves: #14, #11, #16

--- src/pages/mypage/AddScheduleData.ts
+++ src/pages/mypage/AddScheduleData.ts
@@ -4,48 +4,45 @@ export interface AddScheduleType {
   scheduleSummary: string;
   scheduleIcon: string;
   scheduleIconColor: string;
+  notAvailable: string;
   type: string;
 }
 
 export const addScheduleData: AddScheduleType[] = [
   {
     scheduleIndex: 0,
-    scheduleTitle: '예상 월경일 알아보기',
-    scheduleSummary: '예상 월경일을 계산해드려요.',
-    scheduleIcon: 'menstruation',
-    scheduleIconColor: '#FD4E53',
-    type: 'Menstruation',
-  },
-  {
-    scheduleIndex: 1,
     scheduleTitle: '병원 예약 등록하기',
     scheduleSummary: '병원 방문을 잊지 않도록, 알람을 보내드려요.',
     scheduleIcon: 'hospital',
     scheduleIconColor: '#2F6BE2',
+    notAvailable: '로그인하고 병원 방문 시간을 기록하세요.',
     type: 'hospital',
   },
   {
-    scheduleIndex: 2,
+    scheduleIndex: 1,
     scheduleTitle: '의약품 복용 시간 등록하기',
     scheduleSummary: '복용 시간이 되면, 알람을 보내드려요.',
     scheduleIcon: 'medicine',
     scheduleIconColor: '#ABDB98',
+    notAvailable: '로그인하고 복용시간을 기록하세요.',
     type: 'medicine',
   },
   {
-    scheduleIndex: 3,
+    scheduleIndex: 2,
     scheduleTitle: '운동 계획 등록하기',
     scheduleSummary: '운동 계획을 세우고, 일정에 등록해보세요.',
     scheduleIcon: 'exercise',
     scheduleIconColor: '#FBCD29',
+    notAvailable: '로그인하고 운동 계획을 등록하세요.',
     type: 'exercise',
   },
   {
-    scheduleIndex: 4,
+    scheduleIndex: 3,
     scheduleTitle: '기타 일정 등록하기',
     scheduleSummary: '원하는 일정을 등록하세요.',
     scheduleIcon: 'etc',
     scheduleIconColor: '#A07EFF',
+    notAvailable: '원하는 일정을 등록하세요.',
     type: 'etc',
   },
 ];
